ca013c51b25ffc2a040e32656eb3f07080d7731f
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-11-06 23:15:33 +0530

    react router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return (
@@ -8,10 +9,10 @@ function Footer(props) {
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/">About</a></li>
-                            <li><a href="/">Menu</a></li>
-                            <li><a href="contactus.html">Contact</a></li>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/aboutus">About Us</Link></li>
+                            <li><Link to="/menu">Menu</Link></li>
+                            <li><Link to="/contactus">Contact Us</Link></li>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
@@ -22,8 +23,7 @@ function Footer(props) {
                             HONG KONG<br />
                             <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
                             <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                                confusion@food.net</a>
+                            <i className="fa fa-envelope fa-lg"></i>:<a href="mailto:confusion@food.net">confusion@food.net</a>
                         </address>
                     </div>
                     <div className="col-12 col-sm-4 align-self-center">
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <>
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg">Home</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg">About Us</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg">Menu</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg">Contact Us</span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
-                {/*Jumbotron starts*/}
-                    <div className="container-fluid text-sm-center p-5 bg-light">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1 className="display-2">Ristorante Con Fusion</h1>
-                                <p className="lead">We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
 
-                            </div>
+                {/*Jumbotron starts*/}
+                <div className="container-fluid text-sm-center p-5 bg-light">
+                    <div className="row row-header">
+                        <div className="col-12 col-sm-6">
+                            <h1 className="display-2">Ristorante Con Fusion</h1>
+                            <p className="lead">We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+
                         </div>
                     </div>
+                </div>
                 {/*Jumbotron ends*/}
+                
             </>
         );
     }
